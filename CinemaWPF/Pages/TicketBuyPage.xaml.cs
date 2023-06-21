1822d6485f70b554ff2765c889f2cbe00217dad9
Ruslana <garaeva-r_23@mail.ru>
2023-06-22 01:01:17 +0300

    Logic of the ticket buy page

--- CinemaWPF/Pages/TicketBuyPage.xaml.cs
+++ CinemaWPF/Pages/TicketBuyPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CinemaWPF.Core;
+using CinemaWPF.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace CinemaWPF.Pages
 {
@@ -23,6 +26,31 @@ namespace CinemaWPF.Pages
         public TicketBuyPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbNumber.Text != "" && tbName.Text != "" && 
+                tbValidityPeriod.Text != "" && tbCVC.Text != "")
+            {
+                DataBase.MongoDataBase.CurrentTicket = new Ticket()
+                {
+                    MovieName = DataBase.MongoDataBase.CurrentMovie.Name,
+                    Poster = DataBase.MongoDataBase.CurrentMovie.Poster,
+                    HallName = DataBase.MongoDataBase.CurrentHall.Name,
+                    PlaceNumber = DataBase.MongoDataBase.CurrentPlace,
+                    Date = DataBase.MongoDataBase.CurrentSession.Time.ToString("d"),
+                    Time = DataBase.MongoDataBase.CurrentSession.Time.ToString("t"),
+                    User = DataBase.MongoDataBase.CurrentUser,
+                    DateTimeCreate = DataBase.MongoDataBase.CurrentSession.Time
+                };
+
+                _ = DataBase.MongoDataBase.AddTicketToDataBase(DataBase.MongoDataBase.CurrentTicket);
+                MessageBox.Show("Билет добавлен в список Ваших билетов");
+                NavClass.NextPage(new NavComponentsClass("КАТАЛОГ", new InitialPage()));
+            }
+            else
+                MessageBox.Show("Для завершения покупки билета все поля должны быть заполнены!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
