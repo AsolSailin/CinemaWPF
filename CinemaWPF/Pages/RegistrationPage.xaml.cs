02c1fa2a19c3818856ebcd93b8a9cc8d70d93753
Ruslana <garaeva-r_23@mail.ru>
2023-06-22 00:50:37 +0300

    Logic of the registration page

--- CinemaWPF/Pages/RegistrationPage.xaml.cs
+++ CinemaWPF/Pages/RegistrationPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CinemaWPF.Core;
+using CinemaWPF.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,43 @@ namespace CinemaWPF.Pages
         public RegistrationPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbSurname.Text != "" && tbName.Text != "" && tbPatronymic.Text != "" && 
+                tbPhoneNumber.Text != "" && tbEMail.Text != "" && tbLogin.Text != "" && 
+                tbPassword.Text != "" && tbPasswordRepeat.Text != "")
+            {
+                if (DataBase.MongoDataBase.FindByUserLogin(tbLogin.Text) == null)
+                {
+                    var createUser = new User()
+                    {
+                        Surname = tbSurname.Text,
+                        Name = tbName.Text,
+                        Patronymic = tbPatronymic.Text,
+                        EMail = tbEMail.Text,
+                        PhoneNumber = tbPhoneNumber.Text,
+                        Role = "Client",
+                        Login = tbLogin.Text,
+                        Password = tbPassword.Text,
+                        PasswordRepeat = tbPasswordRepeat.Text
+                    };
+
+                    if (tbPassword.Text == tbPasswordRepeat.Text)
+                    {
+                        _ = DataBase.MongoDataBase.AddUserToDataBase(createUser);
+                        MessageBox.Show("Вы были зарегистрированы");
+                        NavClass.NextPage(new NavComponentsClass("АВТОРИЗАЦИЯ", new AuthorizationPage()));
+                    }
+                    else
+                        MessageBox.Show("Пароли не совпадают!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                    MessageBox.Show("Пользователь с таким логином уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+                MessageBox.Show("Для создания аккаунта все поля должны быть заполнены!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
