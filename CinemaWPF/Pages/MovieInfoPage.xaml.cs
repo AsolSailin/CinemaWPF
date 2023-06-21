f3a2e1751c123c8d40cd59c815f54aa8d1d6b505
Ruslana <garaeva-r_23@mail.ru>
2023-06-22 02:23:24 +0300

    Dynamic buttons on the movie information page

--- CinemaWPF/Pages/MovieInfoPage.xaml.cs
+++ CinemaWPF/Pages/MovieInfoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CinemaWPF.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,22 @@ namespace CinemaWPF.Pages
         {
             InitializeComponent();
             GetTextBoxValue();
+
+            if (DataBase.MongoDataBase.CurrentUser.Role == "Admin")
+            {
+                btnSessions.Visibility = Visibility.Visible;
+                btnEdit.Visibility = Visibility.Visible;
+                btnDelete.Visibility = Visibility.Visible;
+
+                btnSessions.Click += SessionsBtn_Click;
+                btnEdit.Click += EditBtn_Click;
+                btnDelete.Click += DeleteBtn_Click;
+            }
+            else
+            {
+                btnBuy.Visibility = Visibility.Visible;
+                btnBuy.Click += SessionsBtn_Click;
+            }
         }
 
         public void GetTextBoxValue()
@@ -37,6 +54,28 @@ namespace CinemaWPF.Pages
             tbCountry.Text = DataBase.MongoDataBase.CurrentMovie.Country;
             tbDuration.Text = DataBase.MongoDataBase.CurrentMovie.Duration;
             tbDescription.Text = DataBase.MongoDataBase.CurrentMovie.Description;
+        }
+
+        private void SessionsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavClass.NextPage(new NavComponentsClass("СЕАНСЫ", new SessionTimePage()));
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavClass.NextPage(new NavComponentsClass("ИЗМЕНЕНИЕ ИНФОРМАЦИИ О ФИЛЬМЕ", new AddingOrEditingMoviePage()));
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите удалить выбранный фильм?", "",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                _ = DataBase.MongoDataBase.DeleteMovie(DataBase.MongoDataBase.CurrentMovie.Id);
+                MessageBox.Show("Фильм был удален");
+                NavClass.NextPage(new NavComponentsClass("КАТАЛОГ", new InitialPage()));
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
